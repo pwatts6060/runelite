7367c43469604a54d446767d8a78b288d76c5e97
pwatts6060 <patrick.watts@hotmail.co.uk>
2021-01-23 19:23:11 -0800

    Start Mining crowdsourcing

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/crowdsourcing/mining/MiningData.java
@@ -0,0 +1,24 @@
+package net.runelite.client.plugins.crowdsourcing.mining;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import net.runelite.api.coords.WorldPoint;
+import net.runelite.client.plugins.crowdsourcing.skilling.SkillingEndReason;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+public class MiningData
+{
+    private final int level;
+    private final int startTick;
+    private final int endTick;
+    private final List<Integer> oreTicks;
+    private final List<Integer> gemTicks;
+    private final int pickaxe;
+    private final int amulet;
+    private final int rockId;
+    private final WorldPoint rockLocation;
+    private final SkillingEndReason reason;
+}
