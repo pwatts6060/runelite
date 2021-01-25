c9ea1844a6104143a1a120dd7fa0106a8ee33b05
pwatts6060 <patrick.watts@hotmail.co.uk>
2021-01-24 16:40:36 -0800

    Reformat code

--- runelite-client/src/main/java/net/runelite/client/plugins/crowdsourcing/mining/CrowdsourcingMining.java
+++ runelite-client/src/main/java/net/runelite/client/plugins/crowdsourcing/mining/CrowdsourcingMining.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, Weird Gloop <admin@weirdgloop.org>
+ * Copyright (c) 2021, Patrick <https://github.com/pwatts6060>
  * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
@@ -34,219 +34,216 @@ import net.runelite.client.eventbus.Subscribe;
 import net.runelite.client.plugins.crowdsourcing.CrowdsourcingManager;
 import net.runelite.client.plugins.crowdsourcing.skilling.SkillingEndReason;
 import net.runelite.client.plugins.crowdsourcing.skilling.SkillingState;
-
 import javax.inject.Inject;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
 
-import static net.runelite.client.plugins.crowdsourcing.mining.RockType.*;
-
 public class CrowdsourcingMining
 {
-    private static final String SWING_MESSAGE = "You swing your pick at the rock.";
-    private static final String INVENTORY_FULL_MESSAGE = "Your inventory is too full to hold";
-    private static final String GEM_MESSAGE = "You just found";
-    private static final Map<Integer, RockType> ORE_OBJECTS = new ImmutableMap.Builder<Integer, RockType>().
-            put(ObjectID.ROCKS_11378, BLURITE).
-            put(ObjectID.ROCKS_11379, BLURITE).
-            put(ObjectID.ROCKS_11161, COPPER).
-            put(ObjectID.ROCKS_10943, COPPER).
-            put(ObjectID.ROCKS_11361, TIN).
-            put(ObjectID.ROCKS_11360, TIN).
-            put(ObjectID.ROCKS_11366, COAL).
-            put(ObjectID.ROCKS_11367, COAL).
-            put(ObjectID.ROCKS_11372, MITHRIL).
-            put(ObjectID.ROCKS_11373, MITHRIL).
-            put(ObjectID.ROCKS_11374, ADAMANT).
-            put(ObjectID.ROCKS_11375, ADAMANT).
-            put(ObjectID.ROCKS_11362, CLAY).
-            put(ObjectID.ROCKS_11363, CLAY).
-            put(ObjectID.ROCKS_11364, IRON).
-            put(ObjectID.ROCKS_11365, IRON).
-            put(ObjectID.ROCKS_11368, SILVER).
-            put(ObjectID.ROCKS_11369, SILVER).
-            put(ObjectID.ROCKS_11370, GOLD).
-            put(ObjectID.ROCKS_11371, GOLD).
-            put(ObjectID.ROCKS_11386, SANDSTONE).
-            put(ObjectID.ROCKS_11387, GRANITE).
-            put(ObjectID.ROCKS_11380, GEMROCK).
-            put(ObjectID.ROCKS_11381, GEMROCK).
-            put(ObjectID.PILE_OF_ROCK, LIMESTONE).
-            put(ObjectID.CRYSTALS, AMETHYST).
-            put(ObjectID.CRYSTALS_11389, AMETHYST).
-            put(ObjectID.ROCKS_11377, RUNITE).
-            put(ObjectID.ROCKS_11376, RUNITE).
-            build();
+	private static final String SWING_MESSAGE = "You swing your pick at the rock.";
+	private static final String INVENTORY_FULL_MESSAGE = "Your inventory is too full to hold";
+	private static final String GEM_MESSAGE = "You just found";
+	private static final Map<Integer, RockType> ORE_OBJECTS = new ImmutableMap.Builder<Integer, RockType>().
+			put(ObjectID.ROCKS_11378, RockType.BLURITE).
+			put(ObjectID.ROCKS_11379, RockType.BLURITE).
+			put(ObjectID.ROCKS_11161, RockType.COPPER).
+			put(ObjectID.ROCKS_10943, RockType.COPPER).
+			put(ObjectID.ROCKS_11361, RockType.TIN).
+			put(ObjectID.ROCKS_11360, RockType.TIN).
+			put(ObjectID.ROCKS_11366, RockType.COAL).
+			put(ObjectID.ROCKS_11367, RockType.COAL).
+			put(ObjectID.ROCKS_11372, RockType.MITHRIL).
+			put(ObjectID.ROCKS_11373, RockType.MITHRIL).
+			put(ObjectID.ROCKS_11374, RockType.ADAMANT).
+			put(ObjectID.ROCKS_11375, RockType.ADAMANT).
+			put(ObjectID.ROCKS_11362, RockType.CLAY).
+			put(ObjectID.ROCKS_11363, RockType.CLAY).
+			put(ObjectID.ROCKS_11364, RockType.IRON).
+			put(ObjectID.ROCKS_11365, RockType.IRON).
+			put(ObjectID.ROCKS_11368, RockType.SILVER).
+			put(ObjectID.ROCKS_11369, RockType.SILVER).
+			put(ObjectID.ROCKS_11370, RockType.GOLD).
+			put(ObjectID.ROCKS_11371, RockType.GOLD).
+			put(ObjectID.ROCKS_11386, RockType.SANDSTONE).
+			put(ObjectID.ROCKS_11387, RockType.GRANITE).
+			put(ObjectID.ROCKS_11380, RockType.GEMROCK).
+			put(ObjectID.ROCKS_11381, RockType.GEMROCK).
+			put(ObjectID.PILE_OF_ROCK, RockType.LIMESTONE).
+			put(ObjectID.CRYSTALS, RockType.AMETHYST).
+			put(ObjectID.CRYSTALS_11389, RockType.AMETHYST).
+			put(ObjectID.ROCKS_11377, RockType.RUNITE).
+			put(ObjectID.ROCKS_11376, RockType.RUNITE).
+			build();
 
+	@Inject
+	private CrowdsourcingManager manager;
 
-    @Inject
-    private CrowdsourcingManager manager;
+	@Inject
+	private Client client;
 
-    @Inject
-    private Client client;
+	private SkillingState state = SkillingState.RECOVERING;
+	private int coolDown;
+	private int lastExperimentEnd = 0;
+	private WorldPoint rockLocation;
+	private int rockId;
+	private RockType rockType;
+	private int startTick;
+	private Pickaxe pickaxe;
+	private int amulet;
+	private boolean ranToRock;
+	private WorldPoint startPoint;
+	private List<Integer> oreTicks = new ArrayList<>();
+	private List<Integer> gemTicks = new ArrayList<>();
 
-    private SkillingState state = SkillingState.RECOVERING;
-    private int coolDown;
-    private int lastExperimentEnd = 0;
-    private WorldPoint rockLocation;
-    private int rockId;
-    private RockType rockType;
-    private int startTick;
-    private Pickaxe pickaxe;
-    private int amulet;
-    private boolean ranToRock;
-    private WorldPoint startPoint;
-    private List<Integer> oreTicks = new ArrayList<>();
-    private List<Integer> gemTicks = new ArrayList<>();
+	private void endExperiment(SkillingEndReason reason)
+	{
+		if (state == SkillingState.MINING)
+		{
+			int endTick = client.getTickCount();
+			int miningLevel = client.getBoostedSkillLevel(Skill.MINING);
+			int pickaxeId = -1;
+			if (pickaxe == null)
+			{
+				Optional<Pickaxe> pickOp = Pickaxe.getPickaxeFromPlayer(client);
+				if (pickOp.isPresent())
+				{
+					pickaxeId = pickOp.get().itemId;
+				}
+			} else
+			{
+				pickaxeId = pickaxe.itemId;
+			}
+			MiningData data = new MiningData(
+					miningLevel,
+					startTick,
+					endTick,
+					oreTicks,
+					gemTicks,
+					ranToRock,
+					pickaxeId,
+					amulet,
+					rockId,
+					rockLocation,
+					reason
+			);
+			manager.storeEvent(data);
 
-    private void endExperiment(SkillingEndReason reason)
-    {
-        if (state == SkillingState.MINING)
-        {
-            int endTick = client.getTickCount();
-            int miningLevel = client.getBoostedSkillLevel(Skill.MINING);
-            int pickaxeId = -1;
-            if (pickaxe == null) {
-                Optional<Pickaxe> pickOp = Pickaxe.getPickaxeFromPlayer(client);
-                if (pickOp.isPresent()) {
-                    pickaxeId = pickOp.get().itemId;
-                }
-            } else {
-                pickaxeId = pickaxe.itemId;
-            }
-            MiningData data = new MiningData(
-                    miningLevel,
-                    startTick,
-                    endTick,
-                    oreTicks,
-                    gemTicks,
-                    ranToRock,
-                    pickaxeId,
-                    amulet,
-                    rockId,
-                    rockLocation,
-                    reason
-            );
-            manager.storeEvent(data);
+			if (!oreTicks.isEmpty() && oreTicks.get(oreTicks.size() - 1) == endTick)
+			{
+				coolDown = 0;
+			} else
+			{
+				coolDown = pickaxe == null ? 8 : pickaxe.maxTicks;
+			}
 
-            if (!oreTicks.isEmpty() && oreTicks.get(oreTicks.size()-1) == endTick) {
-                coolDown = 0;
-            } else {
-                coolDown = pickaxe == null ? 8 : pickaxe.maxTicks;
-            }
+			oreTicks = new ArrayList<>();
+			gemTicks = new ArrayList<>();
+		}
+		pickaxe = null;
+		state = SkillingState.RECOVERING;
+		lastExperimentEnd = client.getTickCount();
+	}
 
-            oreTicks = new ArrayList<>();
-            gemTicks = new ArrayList<>();
-        }
-        pickaxe = null;
-        state = SkillingState.RECOVERING;
-        lastExperimentEnd = client.getTickCount();
-    }
+	@Subscribe
+	public void onChatMessage(ChatMessage event)
+	{
+		final String message = event.getMessage();
+		final ChatMessageType type = event.getType();
+		if (state == SkillingState.CLICKED && type == ChatMessageType.SPAM && message.equals(SWING_MESSAGE))
+		{
+			startTick = client.getTickCount();
+			state = SkillingState.MINING;
+			amulet = Optional.ofNullable(client.getItemContainer(InventoryID.EQUIPMENT))
+					.map(c -> c.getItem(EquipmentInventorySlot.AMULET.getSlotIdx()))
+					.map(Item::getId)
+					.orElse(-1);
+			ranToRock = !client.getLocalPlayer().getWorldLocation().equals(startPoint);
+		} else if (state == SkillingState.MINING && type == ChatMessageType.SPAM && rockType.isSuccessMsg(message))
+		{
+			oreTicks.add(client.getTickCount());
+		} else if (state == SkillingState.MINING && type == ChatMessageType.GAMEMESSAGE && message.equals(INVENTORY_FULL_MESSAGE))
+		{
+			endExperiment(SkillingEndReason.INVENTORY_FULL);
+		} else if (state == SkillingState.MINING && type == ChatMessageType.SPAM && message.startsWith(GEM_MESSAGE))
+		{
+			gemTicks.add(client.getTickCount());
+		}
+	}
 
-    @Subscribe
-    public void onChatMessage(ChatMessage event)
-    {
-        final String message = event.getMessage();
-        final ChatMessageType type = event.getType();
-        if (state == SkillingState.CLICKED && type == ChatMessageType.SPAM && message.equals(SWING_MESSAGE))
-        {
-            startTick = client.getTickCount();
-            state = SkillingState.MINING;
-            amulet = Optional.ofNullable(client.getItemContainer(InventoryID.EQUIPMENT))
-                    .map(c -> c.getItem(EquipmentInventorySlot.AMULET.getSlotIdx()))
-                    .map(Item::getId)
-                    .orElse(-1);
-            ranToRock = !client.getLocalPlayer().getWorldLocation().equals(startPoint);
-        }
-        else if (state == SkillingState.MINING && type == ChatMessageType.SPAM && rockType.isSuccessMsg(message))
-        {
-            oreTicks.add(client.getTickCount());
-        }
-        else if (state == SkillingState.MINING && type == ChatMessageType.GAMEMESSAGE && message.equals(INVENTORY_FULL_MESSAGE))
-        {
-            endExperiment(SkillingEndReason.INVENTORY_FULL);
-        }
-        else if (state == SkillingState.MINING && type == ChatMessageType.SPAM && message.startsWith(GEM_MESSAGE))
-        {
-            gemTicks.add(client.getTickCount());
-        }
-    }
+	@Subscribe
+	public void onGameTick(GameTick tick)
+	{
+		int animId = client.getLocalPlayer().getAnimation();
+		if (state == SkillingState.MINING)
+		{
+			Optional<Pickaxe> pickOp = Pickaxe.getPickaxeFromAnim(animId);
+			if (pickOp.isPresent())
+			{
+				pickaxe = pickOp.get();
+			} else
+			{
+				endExperiment(SkillingEndReason.INTERRUPTED);
+			}
+		} else if (animId != -1)
+		{
+			endExperiment(SkillingEndReason.INTERRUPTED);
+		} else if (state == SkillingState.RECOVERING && client.getTickCount() - lastExperimentEnd >= coolDown)
+		{
+			state = SkillingState.READY;
+		} else if (state == SkillingState.CLICKED && client.getTickCount() - lastExperimentEnd >= 20)
+		{
+			state = SkillingState.READY;
+		}
+	}
 
-    @Subscribe
-    public void onGameTick(GameTick tick)
-    {
-        int animId = client.getLocalPlayer().getAnimation();
-        if (state == SkillingState.MINING)
-        {
-            Optional<Pickaxe> pickOp = Pickaxe.getPickaxeFromAnim(animId);
-            if (pickOp.isPresent())
-            {
-                pickaxe = pickOp.get();
-            }
-            else
-            {
-                endExperiment(SkillingEndReason.INTERRUPTED);
-            }
-        }
-        else if (animId != -1)
-        {
-            endExperiment(SkillingEndReason.INTERRUPTED);
-        }
-        else if (state == SkillingState.RECOVERING && client.getTickCount() - lastExperimentEnd >= coolDown)
-        {
-            state = SkillingState.READY;
-        }
-        else if (state == SkillingState.CLICKED && client.getTickCount() - lastExperimentEnd >= 20)
-        {
-            state = SkillingState.READY;
-        }
-    }
+	@Subscribe
+	public void onWidgetLoaded(WidgetLoaded widgetLoaded)
+	{
+		if (widgetLoaded.getGroupId() == 229)
+		{
+			Widget widget = client.getWidget(widgetLoaded.getGroupId(), 0);
+			if (widget == null)
+			{
+				return;
+			}
+			if (widget.getText().startsWith(INVENTORY_FULL_MESSAGE))
+			{
+				endExperiment(SkillingEndReason.INVENTORY_FULL);
+			}
+		}
+	}
 
-    @Subscribe
-    public void onWidgetLoaded(WidgetLoaded widgetLoaded) {
-        if (widgetLoaded.getGroupId() == 229) {
-            Widget widget = client.getWidget(widgetLoaded.getGroupId(), 0);
-            if (widget == null) {
-                return;
-            }
-            if (widget.getText().startsWith(INVENTORY_FULL_MESSAGE)) {
-                endExperiment(SkillingEndReason.INVENTORY_FULL);
-            }
-        }
-    }
+	@Subscribe
+	public void onMenuOptionClicked(MenuOptionClicked menuOptionClicked)
+	{
+		MenuAction menuAction = menuOptionClicked.getMenuAction();
+		int id = menuOptionClicked.getId();
+		if (state == SkillingState.READY && menuAction == MenuAction.GAME_OBJECT_FIRST_OPTION && ORE_OBJECTS.containsKey(id))
+		{
+			state = SkillingState.CLICKED;
+			lastExperimentEnd = client.getTickCount();
+			rockId = id;
+			rockType = ORE_OBJECTS.get(id);
+			rockLocation = WorldPoint.fromScene(client, menuOptionClicked.getActionParam(), menuOptionClicked.getWidgetId(), client.getPlane());
+			startPoint = client.getLocalPlayer().getWorldLocation();
+		} else
+		{
+			endExperiment(SkillingEndReason.INTERRUPTED);
+		}
+	}
 
-    @Subscribe
-    public void onMenuOptionClicked(MenuOptionClicked menuOptionClicked)
-    {
-        MenuAction menuAction = menuOptionClicked.getMenuAction();
-        int id = menuOptionClicked.getId();
-        if (state == SkillingState.READY && menuAction == MenuAction.GAME_OBJECT_FIRST_OPTION && ORE_OBJECTS.containsKey(id))
-        {
-            state = SkillingState.CLICKED;
-            lastExperimentEnd = client.getTickCount();
-            rockId = id;
-            rockType = ORE_OBJECTS.get(id);
-            rockLocation = WorldPoint.fromScene(client, menuOptionClicked.getActionParam(), menuOptionClicked.getWidgetId(), client.getPlane());
-            startPoint = client.getLocalPlayer().getWorldLocation();
-        }
-        else
-        {
-            endExperiment(SkillingEndReason.INTERRUPTED);
-        }
-    }
-
-    @Subscribe
-    public void onGameObjectDespawned(GameObjectDespawned event)
-    {
-        if (state != SkillingState.MINING)
-        {
-            return;
-        }
-        if (rockId == event.getGameObject().getId() && rockLocation.equals(event.getTile().getWorldLocation()))
-        {
-            endExperiment(SkillingEndReason.DEPLETED);
-        }
-    }
+	@Subscribe
+	public void onGameObjectDespawned(GameObjectDespawned event)
+	{
+		if (state != SkillingState.MINING)
+		{
+			return;
+		}
+		if (rockId == event.getGameObject().getId() && rockLocation.equals(event.getTile().getWorldLocation()))
+		{
+			endExperiment(SkillingEndReason.DEPLETED);
+		}
+	}
 }
